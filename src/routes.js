f3ef151b8b10711e5113e612f90c11239fef919f
Rodrigo Rossi <rdrg.rossi@gmail.com>
2020-08-31 13:09:17 -0300

    criação do primeiro model e inserção no BD

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,15 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ message: 'Hello!  asdas ' });
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Rodrigo Rossi',
+    email: 'rodrigo.rossi@fullcustom.com.br',
+    password_hash: '123456',
+  });
+  return res.json(user);
 });
 
 export default routes;
