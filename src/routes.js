f9584b74d17fb98aac033cc5ea07a835f35c6dd9
Rodrigo Rossi <rdrg.rossi@gmail.com>
2020-08-31 13:20:23 -0300

    Cadastro de usuário

--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,8 @@
 import { Router } from 'express';
-import User from './app/models/User';
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-  const user = await User.create({
-    name: 'Rodrigo Rossi',
-    email: 'rodrigo.rossi@fullcustom.com.br',
-    password_hash: '123456',
-  });
-  return res.json(user);
-});
+routes.post('/users', UserController.store);
 
 export default routes;
